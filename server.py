54af3d1f87bdb4d170ab5fc983935a3e2a21d8bd
Michael Jordan <mjthecoder53161@gmail.com>
2022-03-17 14:26:17 +0900

    updated the routers

--- server.py
+++ server.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, request
 
 app = Flask("Web Translator")
 
+
+@app.route("/")
+def renderIndexPage():
+    return render_template('index.html')
+
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
@@ -13,9 +18,6 @@ def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     return translator.french_to_english(textToTranslate)
 
-@app.route("/")
-def renderIndexPage():
-    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
